093ac455f9614a98e75e28599a6ff2f77d315cea
liukun <liu_kun@msn.com>
2016-04-18 22:48:47 +0800

    貌似搞定了路径输入框

--- src/components/FileExplorer/Navigate.js
+++ src/components/FileExplorer/Navigate.js
@@ -2,8 +2,6 @@
  * Created by liukun on 16/4/17.
  * hadoop文件浏览器的导航条
  */
-
-
 import React, { Component,PropTypes } from 'react';
 import { Form,Icon,Input } from 'antd';
 class Navigate extends Component {
@@ -12,13 +10,51 @@ class Navigate extends Component {
         this.state = {isEdit: false};
     }
 
-    changePathClick(path) {
+    componentDidUpdate(prevProps, prevState) {
+        const input = this.refs.pathInput;
+        if (input) {
+            //const dom = findDOMNode(input);
+            const dom = input.getDOMNode();
+            dom.focus();
+            dom.select();
+            //var rtextRange =findDOMNode(input).createTextRange();
+            //rtextRange.moveStart('character',findDOMNode(input).value.length);
+            //rtextRange.collapse(true);
+            //rtextRange.select();
+        }
+    }
+
+    pathClick(path) {
         const {showFileList} = this.props;
         showFileList(path);
     }
 
-    onEditPathClick() {
-        this.setState({isEdit: !this.state.isEdit})
+    beginEditPath() {
+        this.setState({isEdit: !this.state.isEdit});
+    }
+
+    /**
+     *
+     * @param event
+     */
+    endEditPath(event) {
+        if (event.keyCode) {
+            if (event.keyCode === 13) {
+
+                let value = event.target.value;
+                const {showFileList} = this.props;
+                showFileList(value);
+                this.setState({isEdit: false});
+            } else if (event.keyCode == 27) {
+                this.setState({isEdit: false});
+
+            }
+
+        } else {
+            this.setState({isEdit: false})
+
+        }
+
     }
 
     buildPath(currentPath) {
@@ -29,8 +65,11 @@ class Navigate extends Component {
                 if (item.length !== 0) {
                     const pathName = item.length === 0 ? '/' : item;
                     tempPath += item + '/';
-                    return <span onClick={this.changePathClick.bind(this,tempPath.substring(0,tempPath.length-1))}
-                                 className='canClick' key={index}>{pathName} / </span>
+                    return ( <span onClick={this.pathClick.bind(this,tempPath.substring(0,tempPath.length-1))}
+                                   className='canClick' key={index}>
+                                {pathName} /
+                            </span>
+                    );
                 }
             }
         );
@@ -40,31 +79,33 @@ class Navigate extends Component {
             </span>);
     }
 
-    buildTextBox() {
+    buildPathInput() {
         const {currentPath} = this.props.fileList;
         return (
-            <input defaultValue={currentPath} style={{color:'black'}}/>
+            <input
+                defaultValue={currentPath}
+                style={{color:'black',width:'60%'}}
+                onBlur={this.endEditPath.bind(this) }
+                onKeyUp={this.endEditPath.bind(this) }
+                ref='pathInput'
+            />
         )
-
     }
 
     render() {
 
         const {currentPath} = this.props.fileList;
         return (
-
             <div className='navigate-header'>
-                <span className='canClick' onClick={this.onEditPathClick.bind(this)} style={{float:'right'}}>
+                <span className='canClick' onClick={this.beginEditPath.bind(this)} style={{float:'right'}}>
                     编辑 <Icon type='edit'/>
                 </span>
 
-                <span onClick={this.changePathClick.bind(this,'/')} className='canClick' key='/'>
+                <span onClick={this.pathClick.bind(this,'/')} className='canClick' key='/'>
                      <span><Icon type='hdd' className='root'/>hadoop: / </span>
                 </span>
-                {this.state.isEdit ? this.buildTextBox() : this.buildPath(currentPath)}
+                {this.state.isEdit ? this.buildPathInput() : this.buildPath(currentPath)}
             </div>
-
-
         );
     }
 }
