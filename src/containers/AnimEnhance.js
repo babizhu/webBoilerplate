7abe9d4ebc079d0df3ac3772fe606af56653be1a
liukun <liu_kun@msn.com>
2016-05-17 23:18:50 +0800

    处理了一下动画,避免报警告

--- src/containers/AnimEnhance.js
+++ src/containers/AnimEnhance.js
@@ -2,7 +2,7 @@
  * Created by liu_k on 2016/4/18.
  */
 import React, { Component, PropTypes } from 'react'
-import {QueueAnim} from 'antd'
+import QueueAnim from 'rc-queue-anim';
 
 export  const AnimEnhance = (ComposedComponent) => class extends Component {
     //constructor() {
@@ -13,7 +13,12 @@ export  const AnimEnhance = (ComposedComponent) => class extends Component {
     //}
     render() {
         return (
-            <QueueAnim animConfig={{ translateX: [0, 800]}}>
+            <QueueAnim
+                animConfig={[
+            { opacity: [1, 0], translateX: [0, 100] },
+            { opacity: [1, 0], translateX: [0, -100] }
+          ]}
+                       >
 
                 <div key='c'>
                     <ComposedComponent {...this.props} />
